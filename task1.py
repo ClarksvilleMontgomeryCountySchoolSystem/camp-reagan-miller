574c7e8690a42bea425c04d0b16b4d8b51af0b8e
reagan-miller <reagan.miller586@students.cmcss.net>
2026-02-20 13:27:12 -0600

    H

--- task1.py
+++ task1.py
@@ -8,39 +8,39 @@ def main():
 
     #Variable: birth Prompt: In what year was {first} {last} born:
     
-    birth = input(f"Birth Year: ")
+    birth = input(f"In what year was {first} {last} born: ")
 
     #Variable: days Prompt: How many days will {first} attend?
     
-    days = input(f"Camp Duration: days")
+    days = input(f"How many days will {first} attend? ")
 
     #Variable: p_first Prompt: Parent's First Name:
 
-    p_first = input(f"Parent's Name: ")
+    p_first = input(f"Parent's First Name: ")
 
     #Variable: p_last Prompt: Parent's Last Name:
     
-    p_last = input(f"Parent's Name: ")
+    p_last = input(f"Parent's Last Name: ")
 
     #Variable: phone Prompt: Parent's Phone #:
     
-    phone = input(f"Phone Number: ")
+    phone = input(f"Parent's Phone #: ")
 
     #Variable: street Prompt: Street Address:
 
-    street = input(f"Phone Number: ")
+    street = input(f"Street Address: ")
 
     #Variable: city Prompt: City:
 
-    city = input(f"Phone Number: ")
+    city = input(f"City: ")
 
     #Variable: state Prompt: State Abbreviation:
 
-    state = input(f"Phone Number: ")
+    state = input(f"State Abbreviation: ")
 
     #Variable: zip Prompt: Zip Code:
     
-    zip = input(f"Address:\n{street}\n{city}, {state} {zip}")
+    zip = input(f"Zip Code:\n{street}\n{city}, {state} {zip}")
 
 if __name__ == "__main__":
     main()
